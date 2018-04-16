45e225396727bb424568adaca106553a28dcf70c
Hasan Azizul Haque <hasanavi@gmail.com>
2018-04-17 00:13:33 +0100

    fetch orders from server

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import styles from './Order.css';
 
-const order = (props) => (
+const order = (props) => {
+    const ingredients = [];
+
+    for(let ingredient in props.ingredients) {
+        ingredients.push({
+            name: ingredient,
+            amount: props.ingredients[ingredient]}
+        )
+    }
+
+    const ingredientOutput = ingredients.map(ig => {
+        return <span key={ig.name}>{ig.name} {ig.amount}</span>;
+    })
+
+    return (
     <div className={styles.Order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
+        <p>Ingredients: {ingredientOutput}</p>
+        <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
     </div>
-);
+)};
 
 export default order;
