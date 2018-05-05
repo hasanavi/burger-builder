697ae6f0825bd7f0dac3908c7be52290ce9c3448
Hasan Azizul Haque <hasanavi@gmail.com>
2018-05-05 09:48:33 +0100

    setting up reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,16 +2,35 @@ import * as actionTypes from './actions';
 
 
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 2
 };
 
-const reducer = (state = initialState, action) {
+const reducer = (state = initialState, action) => {
     switch(action.type) {
         case actionTypes.ADD_INGREDIENTS:
-            return state
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    // the following is ES6 way dynamically overrise a key of an object
+                    [action.ingredientName] : state.ingredients[action.ingredientName] + 1
+                }
+            };
         case actionTypes.REMOVE_INGREDIENTS:
-            return state;
+        return {
+            ...state,
+            ingredients: {
+                ...state.ingredients,
+                // the following is ES6 way dynamically overrise a key of an object
+                [action.ingredientName] : state.ingredients[action.ingredientName] - 1
+            }
+        };
         default:
             return state;
     }
