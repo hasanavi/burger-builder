e52734719f45850a12a6ac059ff49cb7f0dec3da
Hasan Azizul Haque <hasanavi@gmail.com>
2018-05-06 19:23:44 +0100

    integrate other modules with redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
 import Button from '../../../components/UI/Button/Button';
 import styles from './ContactData.css';
 import axios from '../../../axios-order';
@@ -207,4 +209,11 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ingredients: state.ingredients,
+        totalPrice: state.tatalPrice
+    }
+}
+
+export default connect(mapStateToProps)(ContactData);
